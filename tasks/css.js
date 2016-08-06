92fbba07d79e1516440abe51bad6f66e1a1dc1d3
Paulo Rodrigues <paulinhoerry@gmail.com>
2016-08-06 10:14:52 -0300

    chore(postcss): add font magician

--- tasks/css.js
+++ tasks/css.js
@@ -1,8 +1,9 @@
 module.exports = function(gulp, plugins, config) {
-  var poststylus = require('poststylus');
-  var lost       = require('lost');
-  var rupture    = require('rupture');
-  var rucksack   = require('rucksack-css');
+  var poststylus   = require('poststylus');
+  var lost         = require('lost');
+  var rupture      = require('rupture');
+  var rucksack     = require('rucksack-css');
+  var fontMagician = require('postcss-font-magician');
 
   gulp.task('css', function() {
     gulp.src(config.sourceStyl)
@@ -10,7 +11,7 @@ module.exports = function(gulp, plugins, config) {
         errorHandler: plugins.notify.onError("ERROR: CSS Compilation Failed")
       }))
       .pipe(plugins.stylus({
-        use: [rupture(), poststylus([lost(), rucksack({ autoprefixer: true })])],
+        use: [rupture(), poststylus([lost(), fontMagician(), rucksack({ autoprefixer: true })])],
         compress: false
       }))
       .pipe(plugins.rename('style.css'))
